96ba1c7f5869436c1274030966aa561dac57f1fc
Michelle3334 <michelle.toscano1978@gmail.com>
2021-12-01 07:59:57 +0000

    Update messages and styling for toasts

--- cart/views.py
+++ cart/views.py
@@ -6,7 +6,6 @@ from products.models import Product
 
 def view_cart(request):
     """Cart contents view"""
-
     return render(request, 'cart/cart.html')
 
 
@@ -18,13 +17,8 @@ def add_to_cart(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
-        messages.success(
-            request, f'Updated {product.name} quantity to {cart[item_id]}')
-    else:
-        cart[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your cart')
+    cart[item_id] = quantity
+    messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
