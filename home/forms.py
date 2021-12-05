44f388c7c143823b4fe1357a1358216a4c2ab9ac
Michelle3334 <michelle.toscano1978@gmail.com>
2021-12-05 08:45:29 +0000

    Removed contact page, form, view and url and imbedded Mailchimp contact form

--- home/forms.py
+++
@@ -1,9 +0,0 @@
-"""Form for Contact"""
-from django import forms
-
-
-class ContactForm(forms.Form):
-    """Contact form fields"""
-    name = forms.CharField(required=True)
-    email_address = forms.EmailField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
