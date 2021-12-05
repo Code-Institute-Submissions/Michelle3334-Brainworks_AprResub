5b7f095619cfae7bb0fefd491bb5c971df21a5e7
Michelle3334 <michelle.toscano1978@gmail.com>
2021-12-05 14:41:01 +0000

    Add braintease html, wire up urls and views and add link to home page

--- home/views.py
+++ home/views.py
@@ -4,5 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     """ A view to return the index page """
-
     return render(request, 'home/index.html')
+
+
+def braintease(request):
+    """ A view to return the braintease page """
+    return render(request, 'braintease/braintease.html')
