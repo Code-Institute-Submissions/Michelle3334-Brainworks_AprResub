7767ab55342356a1690faa05b8237b14d4341412
Michelle3334 <michelle.toscano1978@gmail.com>
2021-12-11 12:55:40 +0000

    Fix linting errors

--- products/apps.py
+++ products/apps.py
@@ -1,6 +1,8 @@
+"""Products app"""
 from django.apps import AppConfig
 
 
 class ProductsConfig(AppConfig):
+    """Products app config"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
