6a6d8fe58a499ce8c53355ce8e8d6ccef945b80d
Michelle3334 <michelle.toscano1978@gmail.com>
2022-04-09 11:57:32 +0000

    Add urls.py and blog_post.html, begin set up of views and urls

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+"Blog app urls"
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.BlogPostList, name="blogpost"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+"Blog app views"
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from django.http import HttpResponseRedirect
+from .models import BlogPost
 
-# Create your views here.
+
+def BlogPostList(request):
+    model = BlogPost
+    queryset = BlogPost.objects.order_by("-date_authored")
+    template_name = "blog_post.html"
+    paginate_by = 6
+
+    return render(request, 'blog/blog_post.html')
