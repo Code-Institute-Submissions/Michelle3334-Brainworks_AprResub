76dc1889c35f2f03adfa5934eeb0e3c1903b2855
Michelle3334 <michelle.toscano1978@gmail.com>
2022-04-09 11:09:31 +0000

    Created blog post and comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from profiles.models import UserProfile
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        UserProfile, on_delete=models.DO_NOTHING, related_name='blog')
+    date_authored = models.DateTimeField(auto_now=True)
+    text = models.TextField()
+
+    class Meta:
+        ordering = ['-date_authored']
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    blogpost = models.ForeignKey(
+        BlogPost, on_delete=models.CASCADE, related_name='comment')
+    author = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING)
+    text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_created']
+
+    def __str__(self):
+        return f'Comment {self.text} by {self.author}'
