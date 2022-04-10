87077a7faa3d08845a82766505bca92df5229827
Michelle3334 <michelle.toscano1978@gmail.com>
2022-04-10 11:39:16 +0000

    Wire up url for blog detail page, add initial styling for blog detail page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 "Blog app views"
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 
@@ -12,3 +12,15 @@ def all_blogs(request):
     }
 
     return render(request, 'blog/blog_post.html', context)
+
+
+def blog_detail(request, blog_id):
+    """ A view to show blog details """
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, 'blog/blog_detail.html', context)
