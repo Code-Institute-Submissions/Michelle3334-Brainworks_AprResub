10694f1fc77c163bc4a722e8c892f1835cdc9e40
Michelle3334 <michelle.toscano1978@gmail.com>
2022-04-13 14:26:32 +0000

    Removed rating from product model and all related pages

--- products/models.py
+++ products/models.py
@@ -27,8 +27,6 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
